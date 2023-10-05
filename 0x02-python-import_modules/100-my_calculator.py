7e32f41174e4ffa427638730f0066f859cca3838
abdirahmanmohamedabdi <abdirahmanabdi.ma@gmail.com>
2023-10-05 08:39:05 -0700

    new task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if__name__ == "___main__":
+if __name__ == "___main__":
     """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mu1, div
     import sys
