324b94104dc43bdbec15dcc60e4ee23815eb8a1d
abdirahmanmohamedabdi <abdirahmanabdi.ma@gmail.com>
2023-10-24 15:51:20 -0700

    new task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueErro('size ,ust be >= 0')
+            raise ValueError('size must be >= 0')
         self.__size = value
 
     @property
