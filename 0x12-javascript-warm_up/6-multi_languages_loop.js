2f2389ea6ac33a9b877bd2638115a64d53156ffe
abdirahmanmohamedabdi <abdirahmanmohamedabdi.ma@gmail.com>
2024-01-09 11:49:19 +0000

    new

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-const leng = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; i < 3; i++) {
-  console.log(leng[i]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(line);
 }
